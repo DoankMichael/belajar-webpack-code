3cc9573de8417cdd6695d2decdd157dffc09b7df
DoankMichael <doankmichael79@gmail.com>
2024-06-22 17:01:01 +0700

    Menggunakan tools Html merge untuk memisahkan configurasi development dan production

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,12 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "main.[contenthash].js",
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
     }),
   ],
-  watch: true,
   devtool: false,
   module: {
     rules: [
