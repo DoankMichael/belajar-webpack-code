f859bbe89327dbab75f1d977cb7b281632263526
DoankMichael <doankmichael79@gmail.com>
2024-06-22 16:29:37 +0700

    membuat content hash name javascript berubah2 agar cache bisa merefresh nya dan membuat tempate html dengan plugins webpack agar menampung nama hash javascript yang berubah

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
-const { watch } = require("fs");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "output"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.[contenthash].js",
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
   watch: true,
   devtool: false,
   module: {
